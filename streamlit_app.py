a1b93841dec62ecb4fc65c1b947ded44c9450132
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-26 23:39:26 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,27 @@ Budget2023 = pd.read_csv(path, encoding='latin-1',index_col=0)
 with st.expander("Presupuesto 2023"):
     st.dataframe(Budget2023,use_container_width=True)
     
-    
+import plotly.express as px
+import streamlit as st
 
-# Sample data (random values for demonstration)
-data = np.random.randn(1000)
+df = px.data.gapminder()
 
-# Create a histogram using Plotly's create_distplot
-fig = ff.create_distplot([data], ['Histogram'], bin_size=0.2)
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
 
-# Set the layout for the chart
-fig.update_layout(title='Histogram Example', xaxis_title='Values', yaxis_title='Frequency')
-
-# Display the Plotly chart in the Streamlit app
-st.plotly_chart(fig)
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
