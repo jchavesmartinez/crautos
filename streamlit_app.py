6246e59c498b9ce92806427222a7d6eeba982852
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-26 22:55:49 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,5 @@ df = pd.read_csv(github_csv_url, encoding='latin-1')
 
 data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6]
 fig = px.histogram(data, nbins=5, title="Sample Histogram")
+
+st.plotly_chart(fig, use_container_width=True)
