6a55b43e5b5576cf2025f0ecdf6f0effc2588e61
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-26 22:47:14 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-st.write("""#Jose""")
+
+github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main/MASTERDATA%20-%20LIMPIA.csv'
+
+# Use pandas to read the CSV file from the URL
+df = pd.read_csv(github_csv_url)
