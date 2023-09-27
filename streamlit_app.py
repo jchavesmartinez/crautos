fdf1f220bc04300b544a22beb171208c1ef26050
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-26 23:35:48 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,22 @@
 import streamlit as st
-import plotly.figure_factory as ff
-import numpy as np
+import time
 import pandas as pd
-import requests
+import numpy as np
+import os
+from datetime import datetime, date, time, timedelta, timezone
+import pymongo
+import pandas as pd
+from pandas import json_normalize
+import json
+import harperdb
+
+from datetime import date
+from datetime import datetime
+import time
 
 
-# GitHub CSV file URL
-github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main/MASTERDATA%20-%20LIMPIA.csv'
+path= "https://raw.githubusercontent.com/jchavesmartinez/StreamlitBudget/main/Budget.csv"
+Budget2023 = pd.read_csv(path, encoding='latin-1',index_col=0)
 
-# Function to fetch and load CSV data
-@st.cache
-def load_data(url):
-    response = requests.get(url)
-    data = pd.read_csv(response.text)
-    return data
-
-# Load CSV data
-data = load_data(github_csv_url)
-
-# Display the loaded data
-st.write(data)
-
-# Sample data (random values for demonstration)
-data = np.random.randn(1000)
-
-# Create a histogram using Plotly's create_distplot
-fig = ff.create_distplot([data], ['Histogram'], bin_size=0.2)
-
-# Set the layout for the chart
-fig.update_layout(title='Histogram Example', xaxis_title='Values', yaxis_title='Frequency')
-
-# Display the Plotly chart in the Streamlit app
-#st.plotly_chart(fig)
+with st.expander("Presupuesto 2023"):
+    st.dataframe(Budget2023,use_container_width=True)
