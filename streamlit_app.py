71418ef62041ff61a82fffa6ffa10a295cecfab3
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-26 22:54:56 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
 
 github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main/MASTERDATA%20-%20LIMPIA.csv'
@@ -7,4 +8,5 @@ github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main
 # Use pandas to read the CSV file from the URL
 df = pd.read_csv(github_csv_url, encoding='latin-1')
 
-#st.table(df)
+data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6]
+fig = px.histogram(data, nbins=5, title="Sample Histogram")
