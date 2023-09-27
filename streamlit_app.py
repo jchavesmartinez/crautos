a2816193a282e7904e2217f57aa90d3e36bcba05
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-26 23:27:53 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import plotly.figure_factory as ff
 import numpy as np
+import pandas as pd
+
+
+github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main/MASTERDATA%20-%20LIMPIA.csv'
+
+# Use pandas to read the CSV file from the URL
+df = pd.read_csv(github_csv_url, encoding='latin-1')
 
 # Sample data (random values for demonstration)
 data = np.random.randn(1000)
