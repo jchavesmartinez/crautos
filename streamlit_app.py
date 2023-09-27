cfdbe790d435d504b0e66e75df7f0697f7f3b6d3
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-26 23:07:58 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly.figure_factory as ff
+
 
 
 github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main/MASTERDATA%20-%20LIMPIA.csv'
@@ -8,10 +8,22 @@ github_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main
 # Use pandas to read the CSV file from the URL
 df = pd.read_csv(github_csv_url, encoding='latin-1')
 
-data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6, 6, 6]
-group_labels = ['Group 1']
+import numpy as np
+import plotly.figure_factory as ff
 
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
 fig = ff.create_distplot(
-        data, group_labels, bin_size=[.1, .25, .5])
+        hist_data, group_labels, bin_size=[.1, .25, .5])
 
+# Plot!
 st.plotly_chart(fig, use_container_width=True)
