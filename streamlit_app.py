6c562c3d2178977ab077f3579c1ff936fe8e1136
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-27 00:11:22 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,19 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import harperdb
+import harperdb
 
 
 URL = "https://prueba-arimafintech.harperdbcloud.com"
 USERNAME = "ARIMAFINTECH"
 PASSWORD = "Jccm130199!"
-#db = harperdb.HarperDB(url=URL, username=USERNAME, password=PASSWORD)
+db = harperdb.HarperDB(url=URL, username=USERNAME, password=PASSWORD)
 
-#SCHEMA= str('PRESUPUESTO_FAMILIAR')
-#TABLE1= str('DIARIO')
-#Diario=pd.DataFrame(db.sql("SELECT * FROM {0}.{1}".format(SCHEMA,TABLE1)))
+SCHEMA= str('PRESUPUESTO_FAMILIAR')
+TABLE1= str('DIARIO')
+Diario=pd.DataFrame(db.sql("SELECT * FROM {0}.{1}".format(SCHEMA,TABLE1)))
 
 
 df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
 
-st.dataframe(df)
+st.dataframe(Diario)
