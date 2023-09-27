82d0c74ba7c1ca9619ceaab076f274ea6b3dbb8e
jchavesmartinez <jchavesm2017@gmail.com>
2023-09-27 00:29:01 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import harperdb
-
-import plotly.express as px
-
 
 # Replace 'raw_csv_url' with the URL of the raw CSV file on GitHub
 raw_csv_url = 'https://raw.githubusercontent.com/jchavesmartinez/crautos/main/MASTERDATA%20-%20LIMPIA.csv'
@@ -15,26 +11,21 @@ dfa = pd.read_csv(raw_csv_url, encoding='latin-1')
 st.dataframe(dfa)
 
 
+import plotly.figure_factory as ff
 
-df = px.data.gapminder()
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-fig = px.scatter(
-    df.query("year==2007"),
-    x="gdpPercap",
-    y="lifeExp",
-    size="pop",
-    color="continent",
-    hover_name="country",
-    log_x=True,
-    size_max=60,
-)
+# Group data together
+hist_data = [x1, x2, x3]
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-with tab1:
-    # Use the Streamlit theme.
-    # This is the default. So you can also omit the theme argument.
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-with tab2:
-    # Use the native Plotly theme.
-    st.plotly_chart(fig, theme=None, use_container_width=True)
-    
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
