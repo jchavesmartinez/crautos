54dd109a790b27f5c1a52922ab5a2f379f03425f
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-28 09:34:22 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ try:
             
 
             # Create a sample DataFrame (replace this with your 'df' from the CSV)
-            data2 = {'values': df[option].values}
+            data2 = {'values': df[option2].values}
             df2 = pd.DataFrame(data2)
 
             # Create a histogram using Plotly Express
