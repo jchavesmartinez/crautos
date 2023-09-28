51b63a151eb3fb0275774113ec4da6fca610c0df
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-28 10:46:10 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ try:
                 fechafiltro = st.slider('Año', min(df['Año']), max(df['Año']), (min(df['Año']), max(df['Año'])))
 
             with colfiltros2:
-                preciofiltro = st.slider('Precio', min(df['Precio']), max(df['Precio']), (min(df['Precio']), max(df['Precio'])))
+                preciofiltro = st.slider('Precio', min(df['Precio']), max(df['Precio']), (0,10))
 
         col1, col2 = st.columns([1, 1])
 
