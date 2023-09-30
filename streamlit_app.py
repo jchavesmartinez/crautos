a1f4e17636d79ec2970b033c1ff3a8af0be1ebec
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-29 21:24:38 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,17 +37,14 @@ try:
 
         
         with st.expander("Menu de filtros"):
-            colfiltros1, colfiltros2 = st.columns([1, 1])
 
-            with colfiltros1:
-                fechafiltro = st.slider('Año', min(df['Año']), max(df['Año']), (min(df['Año']), max(df['Año'])))
-                df=df[(df['Año'] >= list(fechafiltro)[0] ) & (df['Año'] < list(fechafiltro)[1])]
+            fechafiltro = st.slider('Año', min(df['Año']), max(df['Año']), (min(df['Año']), max(df['Año'])))
+            df=df[(df['Año'] >= list(fechafiltro)[0] ) & (df['Año'] < list(fechafiltro)[1])]
 
-                st.write(df)
-                preciofiltro = st.slider('Precio (Millones)', float(min(df['Precio'])/1000000), float(max(df['Precio'])/1000000), (float(min(df['Precio']))/1000000,float(max(df['Precio']))/1000000), step=500000/1000000)
+            st.write(df)
+            preciofiltro = st.slider('Precio (Millones)', float(min(df['Precio'])/1000000), float(max(df['Precio'])/1000000), (float(min(df['Precio']))/1000000,float(max(df['Precio']))/1000000), step=500000/1000000)
                 
-
-            preciofiltro = st.slider('Kilometros', int(min(df['Kilometraje'])), int(max(df['Kilometraje'])), (int(min(df['Kilometraje'])),int(max(df['Kilometraje']))), step=10000)
+            kmfiltro = st.slider('Kilometros', int(min(df['Kilometraje'])), int(max(df['Kilometraje'])), (int(min(df['Kilometraje'])),int(max(df['Kilometraje']))), step=10000)
 
             st.markdown('<hr>', unsafe_allow_html=True)
 
