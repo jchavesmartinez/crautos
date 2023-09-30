c578d33483a807d6277f04bd57022a6a71d6f8ba
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-29 21:27:46 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
 
 
             preciofiltro = st.slider('Precio (Millones)', float(min(df['Precio'])/1000000), float(max(df['Precio'])/1000000), (float(min(df['Precio']))/1000000,float(max(df['Precio']))/1000000), step=500000/1000000)
-            df=df[(df['Precio'] >= list(preciofiltro)[0] ) & (df['Precio'] < list(preciofiltro)[1])]
+            df=df[(df['Precio'] >= list(preciofiltro)[0]*1000000 ) & (df['Precio'] < list(preciofiltro)[1]*1000000)]
                 
             #preciofiltro = st.slider('Kilometros', int(min(df['Kilometraje'])), int(max(df['Kilometraje'])), (int(min(df['Kilometraje'])),int(max(df['Kilometraje']))), step=10000)
             #df=df[(df['Kilometraje'] >= list(kmfiltro)[0] ) & (df['Kilometraje'] < list(kmfiltro)[1])]
