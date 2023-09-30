c254eacd3739eac51a68764ca08246f7f02e3e58
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-29 22:40:42 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -398,7 +398,7 @@ try:
         # Update st.session_state.suma with the selected values
         
         st.session_state.suma.append(suma2)
-        runtime.exists()
+        st.button("Reset", type="primary")
 
         # Display the updated value of st.session_state.suma
         st.write(st.session_state.suma)
