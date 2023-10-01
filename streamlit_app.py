ab053651aedb2835756d6a8fd5d8f00bd5b37cc1
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-30 22:35:19 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -281,7 +281,7 @@ try:
         col3, col4, col5, col6, col7 = st.columns(5)
         col3.metric("Carros totales", len(df['Marca']))
         col4.metric("Precio min", int(min(df['Precio'])))
-        col5.metric("Precio promedio", int(mean(df['Precio'])))
+        col5.metric("Precio promedio", int(df['Precio'].mean()))
         col6.metric("Wind", "9 mph", "-8%")
         col7.metric("Humidity", "86%", "4%")
     
