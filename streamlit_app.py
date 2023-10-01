b885c53a959d101fbd5b52d047d26e65c65e55c8
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-09-30 23:10:31 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ try:
             try:
             
                 kmfiltro = st.slider('Kilometros', int(min(df['Kilometraje'])), int(max(df['Kilometraje']))+1, (int(min(df['Kilometraje'])),int(max(df['Kilometraje'])+1)), step=10000)
-                df=df[(df['Kilometraje'] >= list(kmfiltro)[0]) & (df['Kilometraje'] <= list(kmfiltro)[1])]
+                df=df[(df['Kilometraje'] >= list(kmfiltro)[0]) & (df['Kilometraje'] <= list(kmfiltro)[1]+10)]
                 st.write(len(df['Marca']))
             except:
                 st.write('Solo existe un elemento, no es posible filtrar más el kilometraje')
