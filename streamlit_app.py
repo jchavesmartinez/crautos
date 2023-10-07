ff399d9b7aa6660efd3aacd8fa9b098c292be24b
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-07 17:37:19 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -414,7 +414,9 @@ try:
                 
     with tab2:
 
-        st.write(modelo)
+        modelo_completo=modelo
+        modelo_completo["grupo_id"]=str(modelo_completo["Marca"])+str(modelo_completo["MarcaModelo"])+str(modelo_completo["Grupo de años"])
+        st.write(modelo_completo)
 
         modelo = df.groupby(['Marca', 'MarcaModelo', 'Grupo de años']).agg({'Año': 'mean', 'Precio': ['mean', 'count','median','std']}).reset_index()
         modelo.columns = ['Marca', 'MarcaModelo', 'Grupo de años', 'Año_mean', 'Precio_mean', 'Precio_count', 'Precio_median','Precio_std']
