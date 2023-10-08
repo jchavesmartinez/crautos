16ea086cbe0edc73b7e3d0f97d76e6a0e7787501
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-07 20:32:55 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -442,8 +442,8 @@ try:
         modelo['precio_margen_mean']=modelo['Precio_mean']-modelo['Precio']
         modelo['precio_margen_median']=modelo['Precio_median']-modelo['Precio']
 
-        modelo = modelo[modelo['precio_margen_mean'] >= 500000]
-        modelo = modelo[modelo['precio_margen_median'] >= 500000]
+        modelo = modelo[modelo['precio_margen_mean'] >= 700000]
+        modelo = modelo[modelo['precio_margen_median'] >= 700000]
 
         modelo['km_margen_mean']=modelo['Kilometraje']/modelo['KM_mean']*100
         modelo['km_margen_median']=modelo['Kilometraje']/modelo['KM_median']*100
