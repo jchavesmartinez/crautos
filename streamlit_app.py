32068ed347faca8896fa6da9ecca0506643b7977
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-07 18:31:35 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -433,11 +433,11 @@ try:
 
         # Drop duplicate columns
         modelo = modelo.loc[:, ~modelo.columns.duplicated()]
-        modelo['precio_margen_mean']=modelo['Precio']/modelo['Precio_mean']
-        modelo['precio_margen_median']=modelo['Precio']/modelo['Precio_median']
+        modelo['precio_margen_mean']=modelo['Precio']/modelo['Precio_mean']*100
+        modelo['precio_margen_median']=modelo['Precio']/modelo['Precio_median']*100
 
-        modelo = modelo[modelo['precio_margen_mean'] < 1]
-        modelo = modelo[modelo['precio_margen_median'] < 1]
+        modelo = modelo[modelo['precio_margen_mean'] < 100]
+        modelo = modelo[modelo['precio_margen_median'] < 100]
 
         modelo['precio_margen_mean']=modelo['Precio_mean']-modelo['Precio']
         modelo['precio_margen_median']=modelo['Precio_median']-modelo['Precio']
@@ -448,9 +448,9 @@ try:
         modelo['km_margen_mean']=modelo['Kilometraje']/modelo['KM_mean']
         modelo['km_margen_median']=modelo['Kilometraje']/modelo['KM_mean']
 
-        modelo = modelo[modelo['km_margen_mean'] <= 0.8]
-        modelo = modelo[modelo['km_margen_median'] <= 0.8]
-        modelo = modelo[modelo['km_margen_mean'] > 0.001]
+        modelo = modelo[modelo['km_margen_mean'] <= 80]
+        modelo = modelo[modelo['km_margen_median'] <= 80]
+        #modelo = modelo[modelo['km_margen_mean'] > 0.001]
 
         st.write(modelo)
         st.write(len(modelo))
