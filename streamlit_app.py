1f75b381aac0a5feea9a6098fc2efa6bc1a570c9
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-08 23:07:17 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -549,7 +549,7 @@ try:
                                         'Extraccion Dia','Grupo de años_modelo_completo','grupo_id','Visuales por Dia','Año_mean','Grupo de años_modelo','Estado','KM_mean','Precio_mean','Precio_std','precio_margen_mean','precio_margen_median','Precio_relativestd',
                                         'km_margen_mean','km_margen_median','precio_margen_mean%','precio_margen_median%'])
 
-        modelo = modelo[['Marca_modelo', 'MarcaModelo_modelo','Combustible','Transmision','Estilo','Cilindrada','Año', 'Precio','Precio_median','Kilometraje','KM_median','Precio_count','nota_final','factor_marca','factor_precio','factor_año','factor_km','factor_extras','Comentarios']]
+        modelo2 = modelo[['Marca_modelo', 'MarcaModelo_modelo','Combustible','Transmision','Estilo','Cilindrada','Año', 'Precio','Precio_median','Kilometraje','KM_median','Precio_count','nota_final','factor_marca','factor_precio','factor_año','factor_km','factor_extras','Comentarios']]
         st.dataframe(modelo)
 
 
