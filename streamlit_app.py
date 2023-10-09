283ae75f0dbeb800f41343a7f431a2c88139daa8
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-08 21:06:04 -0600

    q

--- streamlit_app.py
+++ streamlit_app.py
@@ -416,8 +416,6 @@ try:
 
         modelo_completo=modelo
         modelo_completo["grupo_id"] = modelo_completo["Marca"].astype(str) + modelo_completo["MarcaModelo"].astype(str) + modelo_completo["Grupo de años"].astype(str)
-        st.write(modelo_completo)
-        st.write(len(modelo_completo))
 
         modelo = df.groupby(['Marca', 'MarcaModelo', 'Grupo de años']).agg({'Año': 'mean','Kilometraje':['mean','median'], 'Precio': ['mean', 'count','median','std']}).reset_index()
         modelo.columns = ['Marca', 'MarcaModelo', 'Grupo de años', 'Año_mean','KM_mean','KM_median', 'Precio_mean', 'Precio_count', 'Precio_median','Precio_std']
