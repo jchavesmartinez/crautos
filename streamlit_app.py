f11269a2317315a3d7bc85d497e7b6e9e53d85fd
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-08 22:46:58 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -546,7 +546,7 @@ try:
         #columns_to_drop = [1, 3]  # Columns 'B' and 'D' by index
         modelo = modelo.drop(modelo.columns[columns_to_count_indices], axis=1)
         modelo = modelo.drop(columns=['Color ext', 'Color int','Puertas','Libre impuestos','Negociable','Recibe','Provincia','Traspaso','Vehiculo_ID','Fecha ingreso','Visualizaciones','MarcaModelo_modelo_completo','Moneda','Marca_modelo_completo',
-                                        'Extraccion Dia','Grupo de años_modelo_completo','grupo_id','Visuales por Dia','Año_mean','Grupo de años_modelo','Estado','KM_mean','Precio_mean','Precio_std','precio_margen_mean','precio_margen_median'])
+                                        'Extraccion Dia','Grupo de años_modelo_completo','grupo_id','Visuales por Dia','Año_mean','Grupo de años_modelo','Estado','KM_mean','Precio_mean','Precio_std','precio_margen_mean','precio_margen_median','Precio_relativestd'])
 
         st.write(modelo)
         st.write(len(modelo))
