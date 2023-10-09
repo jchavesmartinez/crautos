abdd3224d44667dc9ba946dbed865fab0dbecb42
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2023-10-08 22:56:55 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -549,7 +549,7 @@ try:
                                         'Extraccion Dia','Grupo de años_modelo_completo','grupo_id','Visuales por Dia','Año_mean','Grupo de años_modelo','Estado','KM_mean','Precio_mean','Precio_std','precio_margen_mean','precio_margen_median','Precio_relativestd',
                                         'km_margen_mean','km_margen_median','precio_margen_mean%','precio_margen_median%'])
 
-        modelo2 = modelo[['Marca_modelo', 'MarcaModelo_modelo', 'Precio','Kilometraje','Año']]
+        modelo2 = modelo[['Marca_modelo', 'MarcaModelo_modelo', 'Precio','Precio_median','Kilometraje','Año']]
 
         st.write(modelo2)
         st.write(modelo)
